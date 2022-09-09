abd86629c26f17d60d9ffc30bdea1281eae381b6
Ted G <61029718+ruxpin86@users.noreply.github.com>
2022-09-09 10:36:08 -0600

    adding in some console.logs for debug purposes

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  console.log("App executed");
   return (
     <View style={styles.container}>
       <Text>Hello World!</Text>
